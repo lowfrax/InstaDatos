39410b561d1c81153ffe17508e2f28f1762ff622
lowfrax <ksalomon797@gmail.com>
2026-03-27 04:46:14 -0600

    uno

---
+++ InstaDatos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstaDatos
+//
+//  Created by kevin on 27/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstaDatos/InstaDatosApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaDatosApp.swift
+//  InstaDatos
+//
+//  Created by kevin on 27/3/26.
+//
+
+import SwiftUI
+
+@main
+struct InstaDatosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
