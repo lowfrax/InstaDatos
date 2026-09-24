1c72debf7303e73bb3b011dadc133abab9bab50e
ReviaxZ <xalessandro81@gmail.com>
2026-09-23 21:35:13 -0600

    feat delete txt

--- InstaDatos/AppRootView.swift
+++ InstaDatos/AppRootView.swift
@@ -13,7 +13,7 @@ struct AppRootView: View {
                     .environmentObject(supabase)
             }
         }
-        .txtInputAutocapitalization(.never)
+        
         .tint(AppTheme.accent)
         .preferredColorScheme(.light)
     }
